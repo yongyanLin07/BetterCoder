4e16d3c3db58b3ea5187eaf6a74879e9ff9b3f44
cassie-xi35 <xiyuewen35@gmail.com>
2021-08-03 10:56:33 +0100

    Updated models.py: Page,Like,Mark

--- rango/models.py
+++ rango/models.py
@@ -33,6 +33,9 @@ class Page(models.Model):
     title = models.CharField(max_length=TITLE_MAX_LENGTH) 
     url = models.URLField() 
     views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    marks = models.IntegerField(default=0)
+    comments = models.IntegerField(default=0)
     image = models.ImageField(upload_to = 'profile_images',blank=True)
     description = models.CharField(max_length=DESCRIPTION_MAX_LENGTH)
     slug = models.SlugField(unique=True)
@@ -60,3 +63,19 @@ class Comment(models.Model):
     time = models.DateField(auto_now=True) # update comment time directly
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     page = models.OneToOneField(Page,on_delete=models.CASCADE)
+
+class Like(models.Model): # without registration in rango/admin.py
+    time = models.DateField(auto_now=True)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    page = models.OneToOneField(Page,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.page.title + "(liked by " + self.user.username + ")"
+
+class Mark(models.Model):
+    time = models.DateField(auto_now=True)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    page = models.OneToOneField(Page,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.page.title + "(marked by " + self.user.username + ")"
